72caac6f7e4f796d460837881813e449ea0cd53e
LautaP3 <lautape04@gmail.com>
2023-10-01 21:12:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
      fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
          .then((response) => response.json())
          .then((data) => {
+            const abilities = data.abilities.map((ability) => ability.ability.name);
+            const abilitiesList = abilities.join(", ");
              pokemonInfo.innerHTML = `
                 <h2>${data.name.toUpperCase()}</h2>
                 <img src="${data.sprites.front_default}" alt="${data.name}">
                 <p>Altura: ${data.height} decímetros</p>
                 <p>Peso: ${data.weight} hectogramos</p>
+                <p>Habilidades: ${abilitiesList}</p>
             `;
          })
          .catch((error) => {
